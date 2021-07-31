994ab9a2cc475ee38db919fecb1d266444e928ea
jeruu <secondogg@libero.it>
2021-07-31 17:47:16 +0200

    caricamento base py

--- app.py
+++ app.py
@@ -1,8 +1,21 @@
 from flask import Flask
 from flask import request
 from flask import render_template
+import pymongo
+import os
 
 app = Flask(__name__)
+
+
+
+client = pymongo.MongoClient('mongodb://localhost:27017/')
+db = client['NutrAspectProva']
+utenti_collection = db["utenti"]
+
+
+
+
+
 
 
 @app.route('/fx', methods=['GET', 'POST'])
@@ -13,19 +26,34 @@ def fromProvaStampa(email):  # put application's code here
 
 @app.route('/', methods=['GET', 'POST'])
 def fromProva():
+    emailArr=['capocchia@provole.it','nopon@nopon.it']
+
+
+
     #data = request.data()
     email = request.form.get('email')
     password = request.form.get('password')
-    ris = 'email ' + str(email) + ' ' + 'password ' + str(password)
-    print(request.data)
-    if email == 'capocchia@provole.it':
-        return fromProvaStampa(email)
 
-    #print(data)
-    return  render_template('index.html')
+    if email is not None and password is not None:
+        print('POST pieno')
+        print(email)
+        print(password)
+        print(utenti_collection.find_one({"email": email}))
+
+        if utenti_collection.find_one({"email": email}) is not None:
+            return fromProvaStampa(email)
+
+    else:
+        print('POST vuoto')
+    return render_template('index.html')
 
 
 
+
+
+@app.route('/login')
+def carica():
+    return render_template('login.html')
 
 
 if __name__ == '__main__':
